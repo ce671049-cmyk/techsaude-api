405e1f0bee80b621a8210e1ee12e15743d99b1d2
ce671049-cmyk <ce671049@gmail.com>
2025-11-09 22:33:37 -0300

    Create db.js

---
+++ api/db.js
@@ -0,0 +1,13 @@
+import mysql from "mysql2/promise";
+
+export async function connectDB() {
+  const connection = await mysql.createConnection({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    ssl: process.env.DB_SSL === "true" ? { rejectUnauthorized: true } : false,
+  });
+  return connection;
+}
