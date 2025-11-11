994f9bca49a7e88c9364e5d29c8fd4e1a93741cb
ce671049-cmyk <ce671049@gmail.com>
2025-11-11 08:35:19 -0300

    Create teste_insert.js

---
+++ api/teste_insert.js
@@ -0,0 +1,36 @@
+import mysql from "mysql2/promise";
+
+async function insert() {
+import { connectDB } from "./db.js";
+  });
+
+  const query = `
+    INSERT INTO TB_Usuario (
+      nome_completoUsuario, cpfUsuario, emailUsuario,
+      data_nascUsuario, enderecoUsuario, sexoUsuario,
+      senhaUsuario, telefoneUsuario
+    ) VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+  `;
+
+  const params = [
+    "Carlos Eduardo",
+    "123.456.789-10",
+    "carlos@gmail.com",
+    "26/10/2006",
+    "Rua Exemplo",
+    "M",
+    "123",
+    "(11) 97777-1111",
+  ];
+
+  try {
+    const [result] = await connection.execute(query, params);
+    console.log("✅ Usuário inserido:", result);
+  } catch (err) {
+    console.error("❌ Erro no INSERT:", err);
+  } finally {
+    await connection.end();
+  }
+}
+
+insert();
